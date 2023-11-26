9a7d09e83f57640de184a3854bca289d3cb4fac9
Yash <yashkrgola9012@gmail.com>
2023-11-26 18:26:54 +0530

    Added j12_array.java

---
+++ j12_array.java
@@ -0,0 +1,8 @@
+public class j12_array {
+    public static void main(String[] args) {
+        String[] cars = { "Maruti", "BMW", "Ford", "Mahindra" };
+        for (int i = 0; i < cars.length; i++) {
+            System.out.println(cars[i]);
+        }
+    }
+}
