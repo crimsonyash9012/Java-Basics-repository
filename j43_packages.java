1d1aa7a924c6a08e626032549af8b5a2dd846432
Yash <yashkrgola9012@gmail.com>
2023-12-12 15:38:17 +0530

    Added j43_packages.java

---
+++ j43_packages.java
@@ -0,0 +1,12 @@
+import java.util.*; // import everthing from java.util
+
+
+public class j43_packages {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+        sc.close();
+    }
+}
