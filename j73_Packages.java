3f65b1cbf62946e5cb3cbeb789224c9e606d5ef7
Yash <yashkrgola9012@gmail.com>
2024-01-04 12:03:48 +0530

    Packages in java

---
+++ j73_Packages.java
@@ -0,0 +1,11 @@
+import java.util.*; // import everything from java util
+
+public class j73_Packages {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+        sc.close();
+    }
+}
