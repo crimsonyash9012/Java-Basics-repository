f4a98ca4d22cd99f639bed8ff51ac832c9b19a0b
Yash <yashkrgola9012@gmail.com>
2024-01-07 20:58:06 +0530

    Linked List - Reverse Iterate

---
+++ j84_LinkedList_reverseIterate.java
@@ -0,0 +1,100 @@
+public class j84_LinkedList_reverseIterate {
+    Node head;
+    private int size;
+
+    j84_LinkedList_reverseIterate(){
+        size=0;
+    }
+    public class Node {
+        String data;
+        Node next;
+
+        Node(String data) {
+            this.data = data;
+            this.next = null;
+            size++;
+        }
+
+    }
+
+    public void reverseIterate(){
+        Node prevNode = head;
+        Node currNode = head.next;
+        while(currNode!=null){
+            Node nextNode = currNode.next;
+            currNode.next = prevNode;
+
+            prevNode = currNode;
+            currNode = nextNode;
+        }
+        head.next = null;
+        head = prevNode;
+
+    }
+
+    public void addFirst(String data){
+        Node node = new Node(data);
+//        size++;
+
+        if(head==null){
+            head = node;
+            return; // signifies that the function is ended even if it's void
+        }
+
+        node.next = head;
+        head = node;
+
+    }
+
+    public void addLast(String data){
+        Node newNode = new Node(data);
+
+        if(head==null){
+            head = newNode;
+            return;
+        }
+//        size++;
+
+        Node lastNode = head;
+
+        while(lastNode.next!=null){
+            lastNode = lastNode.next;
+        }
+        lastNode.next = newNode;
+    }
+
+    public void printList(){
+        Node currNode = head;
+
+        if(head==null){
+            System.out.println("List is empty");
+            return;
+        }
+
+        while(currNode!=null){
+            System.out.print(currNode.data + " -> ");
+            currNode = currNode.next;
+        }
+
+        System.out.println("null");
+    }
+
+    public int getSize(){
+        return size;
+    }
+
+    public static void main(String[] args) {
+        j84_LinkedList_reverseIterate LL = new j84_LinkedList_reverseIterate();
+        LL.addFirst("yash");
+        LL.addFirst("is");
+        LL.addFirst("This");
+        LL.addLast("gola");
+        LL.printList();
+
+        LL.reverseIterate();
+
+        LL.printList();
+
+        System.out.println(LL.getSize());
+    }
+}
