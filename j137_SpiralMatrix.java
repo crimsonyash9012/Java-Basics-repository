1e71d6bd3456938d1e29f5388c3d52f28017e42d
Yash <yashkrgola9012@gmail.com>
2024-02-15 15:03:44 +0530

    DSA - Spiral Matrix

---
+++ j137_SpiralMatrix.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class j137_SpiralMatrix {
+
+    public static List<Integer> spiralOrder(int[][] matrix) {
+        int m=matrix.length;//rows
+        int n=matrix[0].length;//columns
+        int left=0;
+        int right=n-1;
+        int top=0;
+        int bottom=m-1;
+        ArrayList<Integer> list=new ArrayList<>();
+        while(top<=bottom && left<=right)
+        {
+            for(int i=left;i<=right;i++)
+            list.add(matrix[top][i]);
+            top++;
+            for(int i=top;i<=bottom;i++)
+            list.add(matrix[i][right]);
+            right--;
+            if(top<=bottom)
+            {
+            for(int i=right;i>=left;i--)
+            list.add(matrix[bottom][i]);
+            bottom--;
+            }
+            if(left<=right)
+            {
+            for(int i=bottom;i>=top;i--)
+            list.add(matrix[i][left]);
+            left++;
+            }
+        }
+        return list; 
+    }
+    public static void main(String[] args) {
+        int [][]matrix = {{1,2,3,4},{5,6,7,8},{9,10,11,12}};
+        List<Integer> abc = spiralOrder(matrix);
+        System.out.println(abc);
+    }
+}
