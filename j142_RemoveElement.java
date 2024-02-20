bf558501e7a20e11dd93acffab93902c494a6a78
Yash <yashkrgola9012@gmail.com>
2024-02-20 19:23:28 +0530

    DSA - Remove Element

---
+++ j142_RemoveElement.java
@@ -0,0 +1,19 @@
+public class j142_RemoveElement {
+    public static int removeElement(int[] nums, int val) {
+        int n=nums.length;
+        int k=0;
+        
+        for(int i=0; i<n; i++){
+            if(nums[i]!=val){
+                nums[k]=nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+    public static void main(String[] args) {
+        int [] nums = {3,2,2,3};
+        int val = 3;
+        System.out.println(removeElement(nums, val));
+    }    
+}
