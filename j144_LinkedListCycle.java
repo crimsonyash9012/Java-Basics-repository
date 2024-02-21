fd6752622da4d37dc5ad78a3fa88f7c33f98e01f
Yash <yashkrgola9012@gmail.com>
2024-02-21 20:21:45 +0530

    DSA - Linked List Cycle

---
+++ j144_LinkedListCycle.java
@@ -0,0 +1,38 @@
+public class j144_LinkedListCycle {
+    static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    public static boolean hasCycle(ListNode head) {
+        ListNode hare = head;
+        ListNode turtle = head;
+
+        if(head==null){
+            return false;
+        }
+
+        while(hare!=null && hare.next!=null){
+            hare = hare.next.next;
+            turtle = turtle.next;
+
+            if(hare == turtle){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(3);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(0);
+        head.next.next.next = new ListNode(-4);
+        head.next.next.next.next = head.next;
+        System.out.println(hasCycle(head));
+
+    }
+}
